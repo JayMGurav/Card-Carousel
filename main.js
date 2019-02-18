ea213126d50e0142362e93aa6fdb6f51f9ff007d
Jay <mrithyunjay2799@gmail.com>
2019-02-18 17:50:03 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,195 @@
+function zero(){
+    $("#card1").css("z-index","9999");
+    $("#card2").css("z-index","999");
+    $("#card3").css("z-index","99");
+    $("#card4").css("z-index","9");
+    $("#card5").css("z-index","999");
+    $("#card6").css("z-index","99");
+    $("#card7").css("z-index","9");
+    $("#card1").css("transform","translateX(0px) scale(2.0)");
+    $("#card2").css("transform","translateX(-200px) scale(1.5)");
+    $("#card3").css("transform","translateX(-300px) scale(1.0)");
+    $("#card4").css("transform","translateX(-400px) scale(0.5)");
+    $("#card5").css("transform","translateX(200px) scale(1.5)");
+    $("#card6").css("transform","translateX(300px) scale(1.0)");
+    $("#card7").css("transform","translateX(400px) scale(0.5)");
+}
+
+function one(){
+    $("#card5").css("z-index","9999");
+    $("#card6").css("z-index","999");
+    $("#card7").css("z-index","99");
+    $("#card4").css("z-index","9");
+    $("#card1").css("z-index","999");
+    $("#card2").css("z-index","99");
+    $("#card3").css("z-index","9");
+    $("#card5").css("transform","translateX(0px) scale(2.0)");
+    $("#card6").css("transform","translateX(200px) scale(1.5)");
+    $("#card7").css("transform","translateX(300px) scale(1.0)");
+    $("#card4").css("transform","translateX(400px) scale(0.5)");
+    $("#card1").css("transform","translateX(-200px) scale(1.5)");
+    $("#card2").css("transform","translateX(-300px) scale(1.0)");
+    $("#card3").css("transform","translateX(-400px) scale(0.5)");
+}
+function two(){
+    $("#card5").css("z-index","999");
+    $("#card6").css("z-index","9999");
+    $("#card7").css("z-index","999");
+    $("#card4").css("z-index","99");
+    $("#card1").css("z-index","99");
+    $("#card2").css("z-index","9");
+    $("#card3").css("z-index","9");
+    $("#card5").css("transform","translateX(-200px) scale(1.5)");
+    $("#card6").css("transform","translateX(0px) scale(2.0)");
+    $("#card7").css("transform","translateX(200px) scale(1.5)");
+    $("#card4").css("transform","translateX(300px) scale(1.0)");
+    $("#card1").css("transform","translateX(-300px) scale(1.0)");
+    $("#card2").css("transform","translateX(-400px) scale(0.5)");
+    $("#card3").css("transform","translateX(400px) scale(0.5)");
+}
+function three(){
+    $("#card5").css("z-index","99");
+    $("#card6").css("z-index","999");
+    $("#card7").css("z-index","9999");
+    $("#card4").css("z-index","999");
+    $("#card1").css("z-index","9");
+    $("#card2").css("z-index","9");
+    $("#card3").css("z-index","99");
+    $("#card5").css("transform","translateX(-300px) scale(1.0)");
+    $("#card6").css("transform","translateX(-200px) scale(1.5)");
+    $("#card7").css("transform","translateX(0px) scale(2.0)");
+    $("#card4").css("transform","translateX(200px) scale(1.5)");
+    $("#card1").css("transform","translateX(-400px) scale(0.5)");
+    $("#card2").css("transform","translateX(400px) scale(0.5)");
+    $("#card3").css("transform","translateX(300px) scale(1.0)");
+}
+function four(){
+    $("#card5").css("z-index","9");
+    $("#card6").css("z-index","99");
+    $("#card7").css("z-index","999");
+    $("#card4").css("z-index","9999");
+    $("#card1").css("z-index","9");
+    $("#card2").css("z-index","99");
+    $("#card3").css("z-index","999");
+    $("#card5").css("transform","translateX(-400px) scale(0.5)");
+    $("#card6").css("transform","translateX(-300px) scale(1.0)");
+    $("#card7").css("transform","translateX(-200px) scale(1.5)");
+    $("#card4").css("transform","translateX(0px) scale(2.0)");
+    $("#card1").css("transform","translateX(400px) scale(0.5)");
+    $("#card2").css("transform","translateX(300px) scale(1.0)");
+    $("#card3").css("transform","translateX(200px) scale(1.5)");
+}
+function five(){
+    $("#card5").css("z-index","9");
+    $("#card6").css("z-index","9");
+    $("#card7").css("z-index","99");
+    $("#card4").css("z-index","999");
+    $("#card1").css("z-index","99");
+    $("#card2").css("z-index","999");
+    $("#card3").css("z-index","9999");
+    $("#card5").css("transform","translateX(400px) scale(0.5)");
+    $("#card6").css("transform","translateX(-400px) scale(0.5)");
+    $("#card7").css("transform","translateX(-300px) scale(1.0)");
+    $("#card4").css("transform","translateX(-200px) scale(1.5)");
+    $("#card1").css("transform","translateX(300px) scale(1.0)");
+    $("#card2").css("transform","translateX(200px) scale(1.5)");
+    $("#card3").css("transform","translateX(0px) scale(2.0)");
+}
+function six(){
+    $("#card5").css("z-index","99");
+    $("#card6").css("z-index","9");
+    $("#card7").css("z-index","9");
+    $("#card4").css("z-index","99");
+    $("#card1").css("z-index","999");
+    $("#card2").css("z-index","9999");
+    $("#card3").css("z-index","999");
+    $("#card6").css("transform","translateX(400px) scale(0.5)");
+    $("#card7").css("transform","translateX(-400px) scale(0.5)");
+    $("#card4").css("transform","translateX(-300px) scale(1.0)");
+    $("#card3").css("transform","translateX(-200px) scale(1.5)");
+    $("#card5").css("transform","translateX(300px) scale(1.0)");
+    $("#card1").css("transform","translateX(200px) scale(1.5)");
+    $("#card2").css("transform","translateX(0px) scale(2.0)");
+}
+function seven(){
+    $("#card5").css("z-index","999");
+    $("#card6").css("z-index","99");
+    $("#card7").css("z-index","9");
+    $("#card4").css("z-index","9");
+    $("#card1").css("z-index","9999");
+    $("#card2").css("z-index","999");
+    $("#card3").css("z-index","99");
+    $("#card6").css("transform","translateX(400px) scale(0.5)");
+    $("#card7").css("transform","translateX(-400px) scale(0.5)");
+    $("#card4").css("transform","translateX(-300px) scale(1.0)");
+    $("#card3").css("transform","translateX(-200px) scale(1.5)");
+    $("#card5").css("transform","translateX(300px) scale(1.0)");
+    $("#card1").css("transform","translateX(0px) scale(2.0)");
+    $("#card2").css("transform","translateX(0px) scale(1.5)");
+}
+$(document).ready(function(){
+   
+    flag=1;
+    $("#next").click(function(){
+        if(flag==0){
+            zero();
+            flag=1;
+        }
+        else if(flag==1){
+            one();
+            flag=2;
+        }
+        else if(flag==2){
+            two();
+            flag=3;
+        }
+        else if(flag==3){
+            three();
+            flag=4;
+        }
+        else if(flag==4){
+            four();
+            flag=5;
+        }
+        else if(flag==5){
+            five();
+            flag=6;
+        }
+        else if(flag==6){
+            six();
+            flag=0;
+        }
+    });
+    $("#prev").click(function(){
+        if(flag==0){
+            six();
+            flag=1;
+        }
+        else if(flag==1){
+            // six();
+            zero()
+            flag=2;
+        }
+        else if(flag==2){
+            one();
+            flag=3;
+        }
+        else if(flag==3){
+            two();
+            flag=4;
+        }
+        else if(flag==4){
+            three();
+            flag=5;
+        }
+        else if(flag==5){
+            four();
+            flag=6;
+        }
+        else if(flag==6){
+            five();
+            flag=0;
+        }
+    });
+});
+
